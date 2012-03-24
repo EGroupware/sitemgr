05e40b9f8ec159d0aee8787c5b3a8075143eb8de
Ralf Becker <ralfbecker@outdoor-training.de>
2012-03-24 12:24:04 +0000

    * SiteMgr: fixed every block had identical id="Mod" validation error

--- sitemgr-site/inc/class.joomla_ui.inc.php
+++ sitemgr-site/inc/class.joomla_ui.inc.php
@@ -406,7 +406,15 @@ class joomla_transformer
 		if ($style) $this->style = $style;
 	}
 
-	public function apply_transform($title,$content)
+	/**
+	 * Apply joomla transformer adding a div with certain class around each block
+	 *
+	 * @param string $title
+	 * @param string $content
+	 * @param Block_SO $block
+	 * @return string html content
+	 */
+	public function apply_transform($title,$content,Block_SO $block)
 	{
 		/**
 		 * @var ui
@@ -418,6 +426,7 @@ class joomla_transformer
 			'content' => $content,
 			'style'   => $this->style,
 			'showtitle' => !empty($title),
+			'id'      => $block->id,
 		);
 		foreach(explode(' ', $this->style) as $style)
 		{
